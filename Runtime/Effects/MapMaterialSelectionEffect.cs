d36550949fefd18f665b5c538763587897f366a6
JonLevin25 <57547561+JonLevin25@users.noreply.github.com>
2020-04-04 22:36:42 +0300

    Add Space Sample (#4)

--- Runtime/Effects/MapMaterialSelectionEffect.cs
+++ Runtime/Effects/MapMaterialSelectionEffect.cs
@@ -5,7 +5,19 @@ namespace WorldMap.Effects
     public class MapMaterialSelectionEffect : MapSelectionEffectBase
     {
         [SerializeField] private Renderer _renderer;
+        [SerializeField] private MaterialKeywordEnabler _materialKeywords;
         [SerializeField] private MaterialEffectColorBlock[] _materialColors;
+        
+#if UNITY_EDITOR
+        private void Start()
+        {
+            // Fix for emission not working in editor until playing with emission in material manually 
+            foreach (var keyword in _materialKeywords.GetKeywords())
+            {
+                _renderer.material.EnableKeyword(keyword);
+            }
+        }
+#endif
         
         public override void Bind(MapEffectState state)
         {
