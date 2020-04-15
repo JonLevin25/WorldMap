c4140df83a9437a6bbc1a14bb48d44aec521ae34
Jonathan Levin <jon.levin25@gmail.com>
2020-04-15 14:38:58 +0300

    Shots for intro of YT intro

---
+++ Assets/DemoCamShotRunner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+
+public class DemoCamShotRunner : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera _virtualCam;
+    [SerializeField] private float _waypointSpeed = 1f;
+    [SerializeField] private List<float> _speedOverrides;
+        
+    private CinemachineTrackedDolly _dollyComponent;
+
+    private void Start()
+    {
+        _dollyComponent = _virtualCam.GetCinemachineComponent<CinemachineTrackedDolly>();
+        _dollyComponent.m_PositionUnits = CinemachinePathBase.PositionUnits.PathUnits;
+        _dollyComponent.m_PathPosition = 0f;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var currSegment = Mathf.FloorToInt(_dollyComponent.m_PathPosition);
+        
+        var speed = GetSpeed(currSegment);
+        var delta = Time.deltaTime * speed;
+        _dollyComponent.m_PathPosition += delta;
+    }
+
+    private float GetSpeed(int segment)
+    {
+        var hasOverride = _speedOverrides?.Count > segment && _speedOverrides[segment] != 0f;
+        return hasOverride ? _speedOverrides[segment] : _waypointSpeed;
+    }
+}
---
+++ Assets/RunForIt.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class RunForIt : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _accel;
+    [SerializeField] private Space _space;
+    [SerializeField] private bool _constDirection;
+
+    private Vector3 _initialDir;
+    private void Start()
+    {
+        _initialDir = GetDirection();
+    }
+    
+    private void Update()
+    {
+        var dir = _constDirection ? _initialDir : GetDirection();
+        var delta = Time.deltaTime * _speed;
+        transform.Translate(delta * dir, _space);
+
+        _speed += Time.deltaTime * _accel;
+    }
+
+    private Vector3 GetDirection()
+    {
+        return (_target.position - transform.position).normalized;
+    }
+}
